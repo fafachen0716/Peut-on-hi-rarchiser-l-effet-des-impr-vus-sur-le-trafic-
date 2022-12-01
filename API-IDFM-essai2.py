933baa0e926703cbc8312303197527b820ea9394
DouId <117099771+DouId@users.noreply.github.com>
2022-12-01 15:51:28 +0100

    API-boucle
    document txt avec les réf arrêts + fichier python

---
+++ API-IDFM-essai2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import os
+import numpy as np
+import pandas as pd
+import io
+import json
+import datetime as pkgdt
+import time
+import requests 
+from requests.adapters import HTTPAdapter
+import itertools # list operators
+
+  
+
+from requests.auth import HTTPBasicAuth
+import requests
+
+file = open('API_STOP.txt', "r")
+line = file.readlines()
+
+for lines in line: 
+# URL de l'API par arrêt
+    url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef='+ str(lines)
+# utilisez readline() pour lire la ligne suivante
+lines = file.readline() 
+
+
+# header doit contenir la clé API
+
+headers = {'Accept':'application/json', 'apikey':'SjlvM6V0HLNnz2SrHK2v5xxVMArX2N5R'}
+
+
+#envoi de la requête au serveur
+req = requests.get(url, headers = headers)
+data = req.json()
+
+#print('Status:',req)
+
+MonitoredStopVisit_list = data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']
+MonitoredVehicleJourney_list = data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['DirectionName']
+StopPointName_list = data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']
+MonitoredCall = data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['DepartureStatus']
+
+for MonitoredStopVisit in MonitoredStopVisit_list:
+    for DirectionName in MonitoredVehicleJourney_list:
+        for StopPointName in StopPointName_list['StopPointName']:
+            for DepartureStatus in MonitoredCall:
+            
+                print ("*********************")
+                print ("Nom de l'opérateur de la ligne : " + MonitoredStopVisit['MonitoredVehicleJourney']['OperatorRef']['value'])
+                print ("Direction du bus : " + DirectionName['value'] )
+                print ("Nom de l'arrêt : "+ StopPointName['value'])
+                print ( "Statut :" + MonitoredCall )
+                
+    
+         
+
+    
+
+open('Reponse.xml', 'wb').write(req.content)
+
+
+
+
+
+
